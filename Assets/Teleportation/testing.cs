8011a50e5eff843f4db7343b1ffc439e46a8b834
robrocop <rohit313.93@gmail.com>
2017-01-22 05:38:08 -0500

    scene is completed with assets and lighting and has been optimized. need to add speech recognition

---
+++ Assets/Teleportation/testing.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing : MonoBehaviour {
+
+	public void printMethod(){
+		if (gameObject.name.Contains("Tour Guide")) {
+			print ("Tour Guide");
+		} else if (gameObject.name.Contains("Ticket Guy")) {
+			Debug.Log ("Ticket guy");
+		} else if (gameObject.name.Contains("Shop Guy")) {
+			Debug.Log ("Shop Guy");
+		}
+	}
+}
